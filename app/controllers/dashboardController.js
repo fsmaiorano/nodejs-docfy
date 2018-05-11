2438092026689087726b68685d7e15109ee6903c
fsmaiorano <fsmaiorano@gmail.com>
2018-05-10 22:10:30 -0300

    update: dashboard view

--- app/controllers/dashboardController.js
+++ app/controllers/dashboardController.js
@@ -1,16 +1,16 @@
-const { Projects, Document } = require('../models');
+const { Project, Document } = require('../models');
 
 module.exports = {
 
   async index(req, res, next) {
     try {
-      const projects = await Projects.findAll({
+      const projects = await Project.findAll({
         include: [Document],
         where: {
           UserId: req.session.user.id,
         },
       });
-      return res.render('dashboard/index', { projects });
+      return res.render('dashboard/index', { projects, user: req.session.user });
     } catch (error) {
       return next(error);
     }
