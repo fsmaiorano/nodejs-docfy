3399afc0979301e2628d23b47a9ef604ad46c6d3
fsmaiorano <fsmaiorano@gmail.com>
2018-05-15 20:52:08 -0300

    update: get correct project and documents,

--- app/controllers/projectController.js
+++ app/controllers/projectController.js
@@ -17,16 +17,19 @@ module.exports = {
 
   async show(req, res, next) {
     try {
+      const selectedProject = parseInt(req.params.projectId);
       const project = await Project.findOne({
         include: [Document],
         where: {
-          UserId: req.session.user.id,
+          id: selectedProject,
         },
       });
 
+      const documents = project.Documents.filter(d => d.ProjectId === selectedProject);
+
       const activeProject = req.params.projectId;
 
-      return res.render('project/index', { project, activeProject });
+      return res.render('project/index', { project, documents, activeProject });
     } catch (err) {
       return next(err);
     }
