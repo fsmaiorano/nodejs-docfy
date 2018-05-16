a529e9ecec0148fa86c783a0c666439685585637
fabio.maiorano <fabio.maiorano@D-CB-001330>
2018-05-16 08:45:00 -0300

    update: adjust route

--- app/controllers/documentController.js
+++ app/controllers/documentController.js
@@ -1,11 +1,30 @@
-const { Document } = require('../models');
+const { Project, Document } = require('../models');
 
 module.exports = {
 
   async show(req, res, next) {
     try {
       const { projectId, documentId } = req.params;
-      let x = 1;
+
+      const project = await Project.findOne({
+        include: [Document],
+        where: {
+          id: projectId,
+        },
+      });
+
+      const documents = project.Documents.filter(d => d.ProjectId === parseInt(req.params.projectId));
+      const selectedDocument = documents.filter(d => d.id === parseInt(documentId));
+
+      const activeDocument = parseInt(req.params.documentId);
+
+      return res.render('project/show', {
+        documents,
+        project,
+        activeDocument: documentId,
+        currentDocument: selectedDocument[0],
+      });
+
     } catch (error) {
       return next(error);
     }
